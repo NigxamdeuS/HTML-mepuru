6f730f22cb63d1dfcf00daca6441b36be27f8de1
NigxamdeuS <to_pon135@icloud.com>
2024-11-09 16:56:32 +0900

    a

--- js/particle.js
+++ js/particle.js
@@ -1,6 +1,6 @@
 $("#particle").particleText({
     text: "Create your own path,<br>even if it's never been walked before.", // 表示させたいテキスト。改行の場合は<br>追加
     colors:["#ffffff ","#ffffff", "#ffffff"], // パーティクルの色を複数指定可能
-    speed: "high",  // slow, middle, high の3つから粒子が集まる速さを選択
+    speed: "higha",  // slow, middle, high の3つから粒子が集まる速さを選択
 });
  
